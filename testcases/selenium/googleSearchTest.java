beb51a844020a618e110bc28e1a6529d002cc8a0
marbokun <marbokun2@gmail.com>
2024-05-04 14:23:57 +0900

    first selenium test

---
+++ testcases/selenium/googleSearchTest.java
@@ -0,0 +1,15 @@
+package browserTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class googleSearchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		driver.close();
+	}
+
+}
